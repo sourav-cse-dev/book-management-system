ea7ec29f9969e92a1dd51d3ce28c45b3233f978e
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-30 22:04:05 +0600

    books can be added and displayed with alert message

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -24,7 +24,6 @@ export class BookComponent {
       this.books.push(newBook);
       this.bookTitle = '';
       this.bookAuthor = '';
-      alert(this.books.length + ' ' + 'books have been added');
     }
   }
 }
